9804e0cd019fcdbd95d08fef9b1681270ec2cf20
maarxx <chris.lewark@gmail.com>
2023-02-20 12:33:29 -0500

    Fixed Thanks Aelanna and PeteTimesSix

--- BetterHemogenFarm/BetterHemoFarmComp.cs
+++ BetterHemogenFarm/BetterHemoFarmComp.cs
@@ -11,7 +11,8 @@ namespace BetterHemogenFarm
 {
     public class BetterHemoFarmComp : ThingComp
     {
-        private Pawn Pawn => (Pawn) this.parent;
+        private Pawn Pawn => (Pawn)this.parent;
+        private bool shouldFarmHemogen;
         public override void CompTick()
         {
             //Log.Error("Let's error on every tick!");
@@ -32,53 +33,13 @@ namespace BetterHemogenFarm
                 command_Toggle2.hotKey = null;
                 //command_Toggle2.icon = TexCommand.ToggleVent;
                 command_Toggle2.icon = ContentFinder<Texture2D>.Get("UI/Commands/AssignOwner");
-                command_Toggle2.isActive = (() => ShouldFarmHemogen);
+                command_Toggle2.isActive = (() => shouldFarmHemogen);
                 command_Toggle2.toggleAction = delegate
                 {
-                    ShouldFarmHemogen = !ShouldFarmHemogen;
+                    shouldFarmHemogen = !shouldFarmHemogen;
                 };
                 yield return command_Toggle2;
             }
-        }
-
-        /// <summary>
-        /// By default props returns the base CompProperties class.
-        /// You can get props and cast it everywhere you use it, 
-        /// or you create a Getter like this, which casts once and returns it.
-        /// Careful of case sensitivity!
-        /// </summary>
-        public BetterHemoFarmCompProperties Props => (BetterHemoFarmCompProperties)this.props;
-
-        public bool ShouldFarmHemogen
-        {
-            get
-            {
-                return Props.shouldFarmHemogen;
-            }
-            set
-            {
-                Props.shouldFarmHemogen = value;
-            }
-        }
-    }
-
-    public class BetterHemoFarmCompProperties : CompProperties
-    {
-        public bool shouldFarmHemogen;
-
-        /// <summary>
-        /// These constructors aren't strictly required if the compClass is set in the XML.
-        /// </summary>
-        public BetterHemoFarmCompProperties()
-        {
-            this.compClass = typeof(BetterHemoFarmComp);
-            this.shouldFarmHemogen = false;
-        }
-
-        public BetterHemoFarmCompProperties(Type compClass) : base(compClass)
-        {
-            this.compClass = compClass;
-            this.shouldFarmHemogen = false;
         }
     }
 }
