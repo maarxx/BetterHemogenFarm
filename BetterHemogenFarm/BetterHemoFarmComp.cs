7c4eb24ed9990aadf5700845b5db65ab760590b0
maarxx <chris.lewark@gmail.com>
2023-02-28 22:02:31 -0500

    Improve Button Text

--- BetterHemogenFarm/BetterHemoFarmComp.cs
+++ BetterHemogenFarm/BetterHemoFarmComp.cs
@@ -73,8 +73,8 @@ namespace BetterHemogenFarm
             if (RecipeDefOf.ExtractHemogenPack.Worker.AvailableOnNow(pawn) && (pawn.IsColonist || pawn.IsPrisonerOfColony))
             {
                 Command_Toggle command_Toggle2 = new Command_Toggle();
-                command_Toggle2.defaultLabel = "HemogenFarmLabel";
-                command_Toggle2.defaultDesc = "HemogenFarmDescription";
+                command_Toggle2.defaultLabel = "Automatically Extract Hemogen";
+                command_Toggle2.defaultDesc = "Automatically place the 'Extract Hemogen Pack' bill on this pawn whenever they meet the following conditions:\n\n- Pawn Already Resting\n- Rest Need Below 40%\n- No Blood Loss condition\n\nIf the bill is not completed by 60% rest, it will be removed, try again the next night. This ensures we only take it when pawns can sleep off the worst of it.";
                 command_Toggle2.hotKey = null;
                 command_Toggle2.icon = DefDatabase<ThingDef>.GetNamed("HemogenPack").uiIcon;
                 command_Toggle2.isActive = (() => shouldFarmHemogen);
